bb3142e364e8025ea6f17bd429d3bb1261e5e798
gsivabalan <122631523+gsivabalan@users.noreply.github.com>
2023-05-09 12:33:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState } from 'react';
 import Dashboard from './components/Dashboard';
 import CreateStudent from './components/CreateStudent';
